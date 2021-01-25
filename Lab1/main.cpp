f1baac35b9c36d23cb3868f499f8158b3a3f9b99
Kaychub <kaychubyt@gmail.com>
2021-01-24 19:42:09 -0700

    Now using pointer and delete

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -21,20 +21,24 @@ int main(int argc, char **argv) {
     }
 
     // Convert enum TokenType to appropriate string (see Token.h)
-    std::string TokenTypeArr[] = {"COMMA", "PERIOD", "Q_MARK", "LEFT_PAREN", "RIGHT_PAREN", "COLON", "COLON_DASH",
+    char const *TokenTypeArr[] = {"COMMA", "PERIOD", "Q_MARK", "LEFT_PAREN", "RIGHT_PAREN", "COLON", "COLON_DASH",
                 "MULTIPLY", "ADD", "SCHEMES", "FACTS", "RULES", "QUERIES", "ID", "STRING",
                 "COMMENT", "UNDEFINED", "EOF"};
 
-    Lexer l = Lexer();
-    l.Run(ss.str());
+    Lexer *l = new Lexer();
+    l->Run(ss.str());
 
     // stdout each token
     // (TOKENTYPE,"name",lineNumber)
-    for (Token* T : l.tokens) {
+    for (Token* T : l->tokens) {
         std::cout << '(' << TokenTypeArr[T->type] << ",\"" << T->name << "\"," << T->lineNumber << ')' << std::endl;
     }
 
-    std::cout << "Total Tokens = " << l.tokens.size();
+    std::cout << "Total Tokens = " << l->tokens.size();
+
+
+    // Clean up
+    delete l;
 
     return 0;
 }
