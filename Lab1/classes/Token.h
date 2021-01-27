2518e0d97a0b3660751ac1d493876cc0e6a8c492
Kaychub <kaychubyt@gmail.com>
2021-01-27 11:33:45 -0700

    Bug fix

--- Lab1/classes/Token.h
+++ Lab1/classes/Token.h
@@ -5,7 +5,7 @@
 
 class Token {
 public:
-    const enum TokenType {COMMA, PERIOD, Q_MARK, LEFT_PAREN, RIGHT_PAREN, COLON, COLON_DASH,
+    enum TokenType {COMMA, PERIOD, Q_MARK, LEFT_PAREN, RIGHT_PAREN, COLON, COLON_DASH,
                     MULTIPLY, ADD, SCHEMES, FACTS, RULES, QUERIES, ID, STRING,
                     COMMENT, UNDEFINED, END}; // END == EOF
                     
